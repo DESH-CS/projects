6917b6acf3f9d67b7cab2eb01b93cb4631a9bd36
shadrackm <ltdpas4@gmail.com>
2024-10-23 00:48:50 +0100

    added project conttent

---
+++ mac.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+                int a=10,b=15,c;
+                printf("\n Hello World");
+                c=a+b;
+                printf("\n a+b=%d",c);
+        return 0;
+}
+
